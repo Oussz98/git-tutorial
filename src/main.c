58215c81f7c7d04044945b02310f2a64f2ff4762
Alex Hoffman <alxhoff@gmail.com>
2020-11-15 20:11:25 +0100

    Coding challenge 2

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
+
 #include <stdio.h>
+#include "espl_lib.h"
 
-int main()
+int main(void)
 {
-   
-   printf("Hello ESPL");
-printf("This is a new print");
 
-   return 0;
+char h;
+int n;
+int i=1;
+
+while(i==1){
+printf("Write a number : \n");
+scanf("%d", &n);
+printf("%s\n", num_to_words(n));
+printf("Wanna play again write r , to quit write q : \n" );
+h = getchar();
+scanf("%c",&h);
+if ( h == 'q') i =0;
+}
 }
